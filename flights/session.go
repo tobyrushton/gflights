c7597c9160b31fd3a31270bf46359b60850a5e5c
tobyrushton <tobyjrushton@icloud.com>
2025-12-04 15:01:19 +0000

    session: initialise

---
+++ flights/session.go
@@ -0,0 +1,55 @@
+package flights
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/browserutils/kooky"
+)
+
+type Session struct {
+	client  *http.Client
+	cookies []string
+}
+
+func getCookies(res *http.Response) ([]string, error) {
+	var cookies []string
+	if setCookie, ok := res.Header["Set-Cookie"]; ok {
+		for _, c := range setCookie {
+			cookies = append(cookies, strings.Split(c, ";")[0])
+		}
+		return cookies, nil
+	}
+	return nil, fmt.Errorf("could not find the 'Set-Cookie' header in the initialization response")
+}
+
+func New() (*Session, error) {
+	client := &http.Client{}
+
+	res, err := client.Get("https://www.google.com")
+	if err != nil {
+		return nil, fmt.Errorf("new session: err sending request to www.google.com: %v", err)
+	}
+
+	cookies, err := getCookies(res)
+	if err != nil {
+		return nil, fmt.Errorf("new session: err getting cookies: %v", err)
+	}
+
+	ctx := context.Background()
+	GOOGLE_ABUSE_EXEMPTION, err := kooky.ReadCookies(ctx, kooky.Valid, kooky.DomainHasSuffix(`google.com`), kooky.Name(`GOOGLE_ABUSE_EXEMPTION`))
+	if err != nil {
+		return nil, fmt.Errorf("new session: err reading GOOGLE_ABUSE_EXEMPTION cookie: %v", err)
+	}
+
+	if len(GOOGLE_ABUSE_EXEMPTION) == 1 {
+		cookies = append(cookies, GOOGLE_ABUSE_EXEMPTION[0].Value)
+	}
+
+	return &Session{
+		client:  client,
+		cookies: cookies,
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.24.0
 
 require google.golang.org/protobuf v1.36.10
 
-require golang.org/x/text v0.31.0 // indirect
+require golang.org/x/text v0.31.0
+
+require github.com/browserutils/kooky v0.2.4 // indirect
