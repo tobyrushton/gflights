7bc8947695edc49e4a8895342ddacc04999ef1d3
tobyrushton <tobyjrushton@icloud.com>
2026-02-10 12:19:11 +0000

    explore: add one way test

--- explore_test.go
+++ explore_test.go
@@ -61,3 +61,37 @@ func TestExplore(t *testing.T) {
 		t.Fatalf("expected to find offers for the following SEA airports: %v", SEAAirports)
 	}
 }
+
+func TestExploreOneWay(t *testing.T) {
+	session, err := gflights.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	args := gflights.ExploreArgs{
+		DepartureDate: time.Now().Add(time.Hour * 24),
+		SrcCities:     []string{"LONDON"},
+		Options: gflights.Options{
+			Travelers: gflights.Travelers{Adults: 1},
+			Currency:  currency.GBP,
+			Stops:     gflights.AnyStops,
+			Class:     gflights.Economy,
+			TripType:  gflights.OneWay,
+		},
+		Coordinates: gflights.ExploreCoordinates{ // SEA
+			NorthLat: 28,
+			EastLng:  141,
+			SouthLat: -11,
+			WestLng:  92,
+		},
+	}
+
+	offers, err := session.GetExplore(context.Background(), args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(offers) == 0 {
+		t.Fatal("expected at least one offer")
+	}
+}
