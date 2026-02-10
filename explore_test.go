5e2723741d6bcf1a169abe51590a95f7fce79048
Toby Rushton <92679304+tobyrushton@users.noreply.github.com>
2026-02-10 15:04:16 +0000

    explore: allow for no coordinates to be provided (#13)

--- explore_test.go
+++ explore_test.go
@@ -193,3 +193,32 @@ func TestMockedExplore(t *testing.T) {
 		t.Fatal("expected at least one offer")
 	}
 }
+
+func TestExploreWithNoCoords(t *testing.T) {
+	session, err := gflights.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	args := gflights.ExploreArgs{
+		DepartureDate: time.Now().Add(time.Hour * 24),
+		ReturnDate:    time.Now().Add(time.Hour * 24 * 7),
+		SrcCities:     []string{"LONDON"},
+		Options: gflights.Options{
+			Travelers: gflights.Travelers{Adults: 1},
+			Currency:  currency.GBP,
+			Stops:     gflights.AnyStops,
+			Class:     gflights.Economy,
+			TripType:  gflights.RoundTrip,
+		},
+	}
+
+	offers, err := session.GetExplore(context.Background(), args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(offers) == 0 {
+		t.Fatal("expected at least one offer")
+	}
+}
